9e2f0b3423eb0d17077d106c3f4dbd6aab5a4202
chiri <chirizxc@proton.me>
2025-11-14 20:44:35 +0300

    add `dumps` and `dumps`

---
+++ tests/test_dumps.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+import yaml_rs
+
+from tests import VALID_YAMLS, normalize_yaml
+
+
+@pytest.mark.parametrize("yaml", VALID_YAMLS)
+def test_valid_yamls_dumps_from_test_suite(yaml: Path) -> None:
+    load_from_str = yaml_rs.loads(yaml.read_text(encoding="utf-8"), parse_datetime=False)
+
+    docs = [load_from_str] if isinstance(load_from_str, dict) else load_from_str
+
+    for doc in docs:
+        parsed_yaml = yaml_rs.loads(normalize_yaml(doc), parse_datetime=False)
+
+        if isinstance(parsed_yaml, set):
+            parsed_yaml = dict.fromkeys(parsed_yaml)
+
+        dump = doc.get("dump")
+        if dump is None:
+            continue
+
+        assert yaml_rs.dumps(parsed_yaml) == dump
